657d0aa0a194e576775de3c5233cd99d434df8e7
Harshit Mehndiratta <65766301+hm90121@users.noreply.github.com>
2024-04-17 17:10:35 +0530

    Create main.tf

---
+++ backend/main.tf
@@ -0,0 +1,19 @@
+
+terraform {                                      
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+resource "aws_instance" "web" {        
+    ami = "ami-0822295a729d2a28e"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "Helloworld"
+    }
+
+}
